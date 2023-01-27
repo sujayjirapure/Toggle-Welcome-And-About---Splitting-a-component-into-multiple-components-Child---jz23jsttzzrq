e13e10502564340e02ecc35180f39604c828f47c
sujay <jirapuresujay@gmail.com>
2023-01-27 13:33:44 +0530

    appok4

--- src/components/App.js
+++ src/components/App.js
@@ -11,17 +11,17 @@ const App = () => {
   return (
     <div id="main">
       {Show ? 
-        <div id="about-div">
-        <h1>
-          Newton School is a neo-university providing highly immersive tech
-          learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
+        <div id="welcome-div">
+          <h1>Welcome to Newton School!!</h1>
+        </div>
       :
-      <div id="welcome-div">
-      <h1>Welcome to Newton School!!</h1>
-    </div>
+        <div id="about-div">
+          <h1>
+            Newton School is a neo-university providing highly immersive tech
+            learning to millions of students, to tap into new-age tech
+            opportunities.
+          </h1>
+        </div>
       }
       <button id="toggle" onClick={toggFn}>Toggle</button>
     </div>
