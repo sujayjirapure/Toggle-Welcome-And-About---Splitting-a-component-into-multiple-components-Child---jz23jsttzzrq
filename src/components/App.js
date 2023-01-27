1e2f845e2fc54b26b027bbdf37889813c4016f08
sujay <jirapuresujay@gmail.com>
2023-01-27 12:56:51 +0530

    appok

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React ,{ useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [Show ,setShow] = useState(false);
+
+  const toggFn = () =>{
+    setShow(true);
+  }
+
   return (
     <div id="main">
-      <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
-        <h1>
-          Newton School is a neo-university providing highly immersive tech
-          learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+      {Show ? 
+        <div id="welcome-div">
+          <h1>Welcome to Newton School!!</h1>
+        </div>
+      :
+        <div id="about-div">
+          <h1>
+            Newton School is a neo-university providing highly immersive tech
+            learning to millions of students, to tap into new-age tech
+            opportunities.
+          </h1>
+        </div>
+      }
+      <button id="toggle" onClick={toggFn}>Toggle</button>
     </div>
   );
 };
